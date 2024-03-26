5d648a2f748ea252d73ab312424bde66f3c3c6e1
Nicolle <nicolleayon@gmail.com>
2024-03-26 12:51:03 -0700

    created Main function to interact with user in console, installed node module package necessary for user input prompt

--- little-library.js
+++ little-library.js
@@ -1,11 +1,22 @@
-// console.log("Hello");
+const prompt = require('prompt-sync')({sigint: true});
+
+
+// Array bookList, will hold Book objects
 const bookList = new Array(); 
 
 class Book {
-    constructor(title, author, isAvailable = 1) {
+    constructor(title, author = "Unknown", isAvailable = 1) {
         this.title  = title, 
         this.author = author, 
         this.isAvailable = isAvailable
+    }
+
+    getBookTitle() {
+        return this.title;
+    }
+
+    getBookAuthor() {
+        return this.author;
     }
 
     getIsAvailable() {
@@ -15,9 +26,7 @@ class Book {
         return "on shelf";
     }
 
-    getBookTitle() {
-        return this.title;
-    }
+
 
     toggleBookStatus(){
         if(this.isAvailable === 0){
@@ -29,26 +38,28 @@ class Book {
     }
 }
 
-//helper function for addBooks
+//Helper function for addBook(): Creates a new book object using Book class and return it
 function createBook(title, author, isAvailable) {
     let newBook = new Book(title, author, isAvailable);
     return newBook;
 }
 
 /* Creates a new book and adds it to the book lists */
-function addBooks (bookTitle, bookAuthor, isBookAvailable) {
+function addBook (bookTitle, bookAuthor, isBookAvailable) {
     //creat new book 
     //add book to map 
     let newBook = createBook(bookTitle, bookAuthor, isBookAvailable);
     bookList.push(newBook);
 }
 
+/* Prints out book title and status from array BookList */
 function printBookList () {  
     bookList.forEach((book) => {
         console.log(book.getBookTitle() + ", " + book.getIsAvailable());
     });
 }
 
+/* Toggles the status of a book with specified parameter 'title' in bookList array */
 function toggleAvailability(title){
     //iterate through array of object, look into each object and see if title matches 
     for(let book of bookList){
@@ -58,11 +69,70 @@ function toggleAvailability(title){
     }
 }
 
+//Interact with user to let user: printBookList(), addBook(), toggleAvailability()
+function Main(){
+    let name = prompt("Welcome to the little library. What is your name? ");
+    let options = Number(0); 
+    console.log(`Nice to meet you ${name}!`);
+
+    while (options !== 4){
+        console.log("What would you like to do? \n1: Print list of books \n2: Add a book \n3: Toggle book availability status \n4: EXIT");
+        options = prompt("Enter choice: ");
+        options = Number(options);
+        console.log("OPTION SELECTED: " + options);
+
+        while(options !== 1 && options !== 2 && options !== 3 && options !== 4){
+            console.log("Invalid choice. Please select number from the list: 1, 2, 3, or 4: ");
+            options = prompt("Enter choice: ");
+            options = Number(options);
+        }
+
+        switch (options) {
+            case 1: 
+                //Print list of books
+                console.log("*** BOOK LIST ***\n");
+                printBookList(); s
+                console.log("");
+                break;
+            case 2:
+                //Add a book
+                let bookTitle = prompt("Please enter the title of the book: ");
+                let bookAuthor = prompt("Please enter the author of the book: ");
+                    if(bookAuthor == ""){
+                        bookAuthor = "Unknown";
+                    }
+                let bookStatus = prompt("Please enter '1' if book is available. Otherwise, please enter '0' if the book is checked out: ");
+                addBook(bookTitle, bookAuthor, bookStatus);
+                //confirm added book
+                for(let book of bookList){
+                    if (book.getBookTitle() === bookTitle && 
+                            book.getBookAuthor() === bookAuthor){
+                        console.log("BOOK ADDED: " + book.getBookTitle() + ", Author: " + book.getBookAuthor() + ", " + book.getIsAvailable());
+                    }
+                }
+                break;
+            case 3:
+                //Toggle book availability status
+                bookTitle = prompt("What is the title of the book you want to toggle availability for?");
+                toggleAvailability(bookTitle);
+                break; 
+            default: 
+                console.log("Goodbye!");
+        }
+    }
+
+ 
+    
+}
 
 
 /* TESTS */
-addBooks("Brown Bear", "", 1);
-addBooks("The Little Prince", "", 0);
-printBookList(); 
-toggleAvailability("Brown Bear");
-printBookList(); 
+Main();
+// addBook("Brown Bear", "", 1);
+// addBook("The Little Prince", "", 0);
+// addBook("Andrew Henry's Meadow");
+// printBookList(); 
+// console.log();
+// toggleAvailability("Andrew Henry's Meadow");
+// printBookList(); 
+// console.log(bookList);
